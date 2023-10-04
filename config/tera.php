a9a2282be4cd44ddeae34b3e637c8dcfb16733db
Rama Noor Rizki <ramanoorrizki@gmail.com>
2023-10-04 11:42:52 +0800

    chore(backend): Add Configuration for tera

---
+++ config/tera.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+  'tum-bbm' => ['jenis' => 'tera_jenis_a'],
+  'timbangan-elektronik-kelas' => ['jenis' => 'tera_jenis_b'],
+  'timbangan-jembatan' => ['jenis' => 'tera_jenis_b'],
+  'timbangan-cepat' => ['jenis' => 'tera_jenis_c'],
+  'timbangan-pegas' => ['jenis' => 'tera_jenis_c'],
+  'timbangan-meja' => ['jenis' => 'tera_jenis_f'],
+  'timbangan-bobot-ingsut' => ['jenis' => 'tera_jenis_c'],
+  'timbangan-sentisimal' => ['jenis' => 'tera_jenis_f'],
+  'pompa-ukur-bbm' => ['jenis' => 'tera_jenis_d'],
+  'dacin' => ['jenis' => 'tera_jenis_c'],
+  'neraca' => ['jenis' => 'tera_jenis_f'],
+  'takaran-basah-kering' => ['jenis' => 'tera_jenis_e'],
+  'meter-kayu' => ['jenis' => 'tera_jenis_c']
+];
