b0dc684e8a0b1ec09e1a6045b03f7d9cd776a6bb
Rama Noor Rizki <ramanoorrizki@gmail.com>
2023-10-30 10:52:30 +0800

    feat(be): get attribute tera and set to layout

--- app/Livewire/PrintTeraPreview.php
+++ app/Livewire/PrintTeraPreview.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
 use Dompdf\Dompdf;
 use Livewire\Component;
 
@@ -9,8 +10,127 @@ class PrintTeraPreview extends Component
 {
     public $tera;
     public $id;
+    public $kode_pengajuan;
+    public $no_surat;
+    public $jenis_tera;
+    public $nama_pemohon;
+    public $alamat_pemohon;
+    public $nama_skhp;
+    public $alamat_skhp;
+    public $kelurahan_skhp;
+    public $kecamatan_skhp;
+    public $kota_skhp;
+    public $nomor_kontak;
+    public $dokumen_surat_permohonan;
+    public $dokumen_skhp_sebelumnya;
+    public $dokumen_bukti_pendukung_lainnya;
+    public $status;
+    public $keterangan;
+    public $tanggal_pengujian;
+    public $tanggal_pengajuan;
+    public $tempat_pengujian;
+    public $alamat_pengujian;
+    public $jumlah_nozzle;
+    public $jumlah_perlengkapan;
+    public $jumlah_uttp;
+
+    public function generateRomanNumberForPoint($point)
+    {
+        switch ($point) {
+            case 'PEMOHON':
+                return 'I';
+            case 'SKHP ATAS NAMA':
+                return 'II';
+            case 'KELENGKAPAN DOKUMEN':
+                return 'III';
+            case 'JUMLAH UTTP':
+                return 'IV';
+            case 'JUMLAH NOZZLE':
+                return 'V';
+            case 'JUMLAH PERLENGKAPAN':
+                return 'V';
+            case 'IDENTITAS UTTP':
+                return $this->getJenisTera() == 'tera_jenis_c' ? 'V' : 'VI';
+            case 'TANGGAL PENGUJIAN':
+                return $this->getJenisTera() == 'tera_jenis_c' ? 'VI' : 'VII';
+            case 'INFORMASI LAINNYA':
+                return $this->getJenisTera() == 'tera_jenis_c' ? 'VII' : 'VIII';
+        }
+    }
+
+    public function getTeraUppercase()
+    {
+        $words = explode('-', $this->tera);
+
+        $sentenceCase = array_map(function ($word) {
+            return strtoupper($word);
+        }, $words);
+
+        return implode(' ', $sentenceCase);
+    }
+
+    public function getTanggalPengujian()
+    {
+        $tanggal = new Carbon($this->tanggal_pengujian);
+        $tanggal->setLocale('id');
+        return $tanggal->isoFormat('dddd, DD MMMM YYYY');
+    }
+
+    public function getTanggalSekarang()
+    {
+        $tanggal = new Carbon();
+        $tanggal->setLocale('id');
+        return $tanggal->isoFormat('DD MMMM YYYY');
+    }
+
+    public function getJenisTera()
+    {
+        return
+            config("tera.$this->tera.jenis");
+    }
+
+    public function getModel()
+    {
+        return
+            config("tera.$this->tera.model_tera");
+    }
+
+    public function getProps($id)
+    {
+        $dataTera = $this->getModel()::find($id);
+        $this->kode_pengajuan = $dataTera->kode_pengajuan;
+        $this->no_surat = $dataTera->no_surat;
+        $this->jenis_tera = $dataTera->jenis_tera;
+        $this->nama_pemohon = $dataTera->nama_pemohon;
+        $this->alamat_pemohon = $dataTera->alamat_pemohon;
+        $this->nama_skhp = $dataTera->nama_skhp;
+        $this->alamat_skhp = $dataTera->alamat_skhp;
+        $this->kelurahan_skhp = $dataTera->kelurahan_skhp;
+        $this->kecamatan_skhp = $dataTera->kecamatan_skhp;
+        $this->kota_skhp = $dataTera->kota_skhp;
+        $this->nomor_kontak = $dataTera->nomor_kontak;
+        $this->dokumen_surat_permohonan = $dataTera->dokumen_surat_permohonan;
+        $this->dokumen_skhp_sebelumnya = $dataTera->dokumen_skhp_sebelumnya;
+        $this->dokumen_bukti_pendukung_lainnya = $dataTera->dokumen_bukti_pendukung_lainnya;
+        $this->jumlah_uttp = $dataTera->jumlah_uttp;
+        if (isset($dataTera->jumlah_nozzle)) {
+            $this->jumlah_nozzle = $dataTera->jumlah_nozzle;
+        }
+        if (isset($dataTera->jumlah_perlengkapan)) {
+            $this->jumlah_perlengkapan = $dataTera->jumlah_perlengkapan;
+        }
+        $this->status = $dataTera->status;
+        $this->keterangan = $dataTera->keterangan;
+        $this->tanggal_pengujian = $dataTera->tanggal_pengujian;
+        $this->tanggal_pengajuan = $dataTera->tanggal_pengajuan;
+        $this->tempat_pengujian = $dataTera->tempat_pengujian;
+        $this->alamat_pengujian = $dataTera->alamat_pengujian;
+    }
+
     public function mount()
     {
+        $id = request()->query('id');
+        $this->getProps($id);
         $this->id = request()->query('id');
     }
 
--- resources/views/livewire/print-tera-preview.blade.php
+++ resources/views/livewire/print-tera-preview.blade.php
@@ -1,8 +1,7 @@
 <div class="preview-container border-dark-grey bg-white">
 
   <div class="button-cetak-container pr-4 mb-2">
-    <button id="cetak-button" href="/pengelolaan-layanan/cetak-tera/{{$tera}}?id={{$id}}"
-      class="button-primary-sm">Cetak</button>
+    <button id="cetak-button" href="/pengelolaan-layanan/cetak-tera/{{$tera}}?id={{$id}}" class="button-primary-sm">Cetak</button>
   </div>
   <div id="ready-to-print" class="mx-4">
     <div class="kop-surat  flex justify-center align-center gap-x-2 border-b-2 pb-4 border-black">
@@ -24,36 +23,41 @@
     <div class="body mx-8">
       <div class="title mt-4 leading-tight">
         <p class="w-fit text-sm font-bold mx-auto text-black">PELAYANAN</p>
-        <p class="w-fit text-sm underline font-bold mx-auto tracking-wide text-black">TERA ULANG METER KAYU</p>
-        <p class="w-fit text-sm font-bold mx-auto text-black">Nomor: 69 / UPT.MET / 10 / 2023</p>
+        <p class="w-fit text-sm underline font-bold mx-auto tracking-wide text-black">TERA ULANG
+          {{$this->getTeraUppercase()}}
+        </p>
+        <p class="w-fit text-sm font-bold mx-auto text-black">Nomor: {{$no_surat}}</p>
       </div>
       <ul>
         <li class="flex mb-4">
-          <div class="point text-black text-sm w-8">I.</div>
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('PEMOHON')}}.</div>
           <div class="point-content">
             <div class="point-head text-black text-sm mb-1">PEMOHON</div>
             <ul>
               <li class="point-body flex">
-                <div class="point-inside w-8 text-black text-sm">1.</div>
+                <div class="point-inside w-8 text-black text-sm">1.
+                </div>
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Nama</p>
                   <p class="divider mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">Alfi Tajungkang</p>
+                  <p class="value text-black text-sm">{{$nama_pemohon}}</p>
                 </div>
               </li>
               <li class="point-body flex">
-                <div class="point-inside w-8 text-black text-sm">2.</div>
+                <div class="point-inside w-8 text-black text-sm">
+                  2.
+                </div>
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Alamat</p>
                   <p class="divider  mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">Jl Ilalang</p>
+                  <p class="value text-black text-sm">{{$alamat_pemohon}}</p>
                 </div>
               </li>
             </ul>
           </div>
         </li>
         <li class="flex mb-4">
-          <div class="point text-black text-sm w-8">II.</div>
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('SKHP ATAS NAMA')}}.</div>
           <div class="point-content">
             <div class="point-head text-black text-sm mb-1">SKHP ATAS NAMA</div>
             <ul>
@@ -62,7 +66,7 @@
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Nama Perusahaan</p>
                   <p class="divider mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">Alfi</p>
+                  <p class="value text-black text-sm">{{$nama_skhp}}</p>
                 </div>
               </li>
               <li class="point-body flex">
@@ -70,7 +74,7 @@
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Alamat</p>
                   <p class="divider  mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">Jl Ilalang</p>
+                  <p class="value text-black text-sm">{{$alamat_skhp}}</p>
                 </div>
               </li>
               <li class="point-body flex">
@@ -78,7 +82,7 @@
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Kelurahan</p>
                   <p class="divider  mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">Banjarbaru Selatan</p>
+                  <p class="value text-black text-sm">{{$kelurahan_skhp}}</p>
                 </div>
               </li>
               <li class="point-body flex">
@@ -86,7 +90,7 @@
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Kecamatan</p>
                   <p class="divider  mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">Loktabat Selatan</p>
+                  <p class="value text-black text-sm">{{$kecamatan_skhp}}</p>
                 </div>
               </li>
               <li class="point-body flex">
@@ -94,7 +98,7 @@
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Kota</p>
                   <p class="divider  mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">Banjarbaru</p>
+                  <p class="value text-black text-sm">{{$kota_skhp}}</p>
                 </div>
               </li>
               <li class="point-body flex">
@@ -102,17 +106,17 @@
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Nomor Kontak Telp/HP</p>
                   <p class="divider  mr-2 text-black text-sm">:</p>
-                  <p class="value text-black text-sm">088245672170</p>
+                  <p class="value text-black text-sm">{{$nomor_kontak}}</p>
                 </div>
               </li>
             </ul>
           </div>
         </li>
         <li class="flex mb-4">
-          <div class="point text-black text-sm w-8">III.</div>
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('KELENGKAPAN DOKUMEN')}}.</div>
           <div class="point-content">
             <div class="flex point-head  mb-2">
-              <p class="text-black w-[13.5rem]  text-sm">KELENGKAPAN DOKUMEN</p>
+              <p class="text-black w-56  text-sm">KELENGKAPAN DOKUMEN</p>
               <p class="text-black ml-4 text-sm">Ada</p>
               <p class="text-black ml-4 text-sm">Tidak Ada</p>
             </div>
@@ -121,53 +125,97 @@
                 <div class="point-inside w-8 text-black text-sm">1.</div>
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48  text-black text-sm">Surat Permohonan</p>
+                  @if ($dokumen_surat_permohonan != null)
                   <div class="checkbox ml-6 border border-black flex justify-center content-center w-5 h-5">
                     <img src="{{ asset('/images/check.png') }}" class="w-4" alt="">
                   </div>
                   <div class="checkbox ml-10 border border-black flex justify-center content-center w-5 h-5">
                   </div>
+                  @else
+                  <div class="checkbox ml-6 border border-black flex justify-center content-center w-5 h-5">
+                  </div>
+                  <div class="checkbox ml-10 border border-black flex justify-center content-center w-5 h-5">
+                    <img src="{{ asset('/images/check.png') }}" class="w-4" alt="">
+                  </div>
+                  @endif
                 </div>
               </li>
               <li class="point-body my-1 flex">
                 <div class="point-inside w-8 text-black text-sm">2.</div>
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">SKHP Sebelumnya</p>
+                  @if ($dokumen_skhp_sebelumnya != null)
                   <div class="checkbox ml-6 border border-black flex justify-center content-center w-5 h-5">
                     <img src="{{ asset('/images/check.png') }}" class="w-4" alt="">
                   </div>
                   <div class="checkbox ml-10 border border-black flex justify-center content-center w-5 h-5">
                   </div>
+                  @else
+                  <div class="checkbox ml-6 border border-black flex justify-center content-center w-5 h-5">
+                  </div>
+                  <div class="checkbox ml-10 border border-black flex justify-center content-center w-5 h-5">
+                    <img src="{{ asset('/images/check.png') }}" class="w-4" alt="">
+                  </div>
+                  @endif
                 </div>
               </li>
               <li class="point-body my-1 flex">
                 <div class="point-inside w-8 text-black text-sm">3.</div>
                 <div class="point-inside-content  text-black text-sm flex">
                   <p class="atribute w-48 text-black text-sm">Bukti Pendukung Lainnya</p>
+                  @if ($dokumen_bukti_pendukung_lainnya != null)
                   <div class="checkbox ml-6 border border-black flex justify-center content-center w-5 h-5">
                     <img src="{{ asset('/images/check.png') }}" class="w-4" alt="">
                   </div>
                   <div class="checkbox ml-10 border border-black flex justify-center content-center w-5 h-5">
                   </div>
+                  @else
+                  <div class="checkbox ml-6 border border-black flex justify-center content-center w-5 h-5">
+                  </div>
+                  <div class="checkbox ml-10 border border-black flex justify-center content-center w-5 h-5">
+                    <img src="{{ asset('/images/check.png') }}" class="w-4" alt="">
+                  </div>
+                  @endif
                 </div>
               </li>
             </ul>
           </div>
         </li>
         <li class="flex mb-4">
-          <div class="point text-black text-sm w-8">IV.</div>
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('JUMLAH UTTP')}}.</div>
           <div class="point-content flex">
-            <p class="atribute w-[13.5rem] text-black text-sm">JUMLAH UTTP</p>
+            <p class="atribute w-56 text-black text-sm">JUMLAH UTTP</p>
             <p class="divider mr-2 text-black text-sm">:</p>
-            <p class="value text-black text-sm">2</p>
+            <p class="value text-black text-sm">{{$jumlah_uttp}}</p>
           </div>
         </li>
+        @if ($this->getJenisTera() == 'tera_jenis_b')
         <li class="flex mb-4">
-          <div class="point text-black text-sm w-8">V.</div>
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('JUMLAH NOZZLE')}}.</div>
+          <div class="point-content flex">
+            <p class="atribute w-56 text-black text-sm">JUMLAH NOZZLE</p>
+            <p class="divider mr-2 text-black text-sm">:</p>
+            <p class="value text-black text-sm">{{$jumlah_nozzle}}</p>
+          </div>
+        </li>
+        @endif
+        @if ($this->getJenisTera() == 'tera_jenis_d')
+        <li class="flex mb-4">
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('JUMLAH PERLENGKAPAN')}}.</div>
+          <div class="point-content flex">
+            <p class="atribute w-56 text-black text-sm">JUMLAH PERLENGKAPAN</p>
+            <p class="divider mr-2 text-black text-sm">:</p>
+            <p class="value text-black text-sm">{{$jumlah_perlengkapan}}</p>
+          </div>
+        </li>
+        @endif
+        <li class="flex mb-4">
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('IDENTITAS UTTP')}}.</div>
           <div class="point-content w-full">
             <div class="point-head text-black text-sm mb-2">IDENTITAS UTTP</div>
             <div class="table w-full">
               <div class="thead w-full flex">
-                <div class="border w-full py-2 pl-1 th border-black font-normal text-black text-sm">No</div>
+                <div class="border w-36 py-2 pl-1 th border-black font-normal text-black text-sm">No</div>
                 <div class="border w-full py-2 pl-1 th border-black font-normal text-black text-sm">Merek</div>
                 <div class="border w-full py-2 pl-1 th border-black font-normal text-black text-sm">Tipe/Model</div>
                 <div class="border w-full py-2 pl-1 th border-black font-normal text-black text-sm">Nomor Seri</div>
@@ -175,7 +223,7 @@
               </div>
               <div class="tbody">
                 <div class="tr flex">
-                  <div class="border w-full py-2 pl-1 th border-black font-normal text-black text-sm">1</div>
+                  <div class="border w-36 py-2 pl-1 th border-black font-normal text-black text-sm">999</div>
                   <div class="border w-full py-2 pl-1 th border-black font-normal text-black text-sm">Yamaha
                   </div>
                   <div class="border w-full th py-2 pl-1 border-black font-normal text-black text-sm">Matic</div>
@@ -187,15 +235,15 @@
           </div>
         </li>
         <li class="flex mb-4">
-          <div class="point text-black text-sm w-8">VI.</div>
+          <div class="point text-black text-sm w-8">{{$this->generateRomanNumberForPoint('TANGGAL PENGUJIAN')}}.</div>
           <div class="point-content flex">
-            <p class="atribute w-[13.5rem] text-black text-sm">Tanggal Pengujian</p>
+            <p class="atribute w-56 text-black text-sm">TANGGAL PENGUJIAN</p>
             <p class="divider mr-2 text-black text-sm">:</p>
-            <p class="value text-black text-sm">Selasa, 12 Oktober 2023</p>
+            <p class="value text-black text-sm">{{$this->getTanggalPengujian()}}</p>
           </div>
         </li>
         <li class="flex mb-4">
-          <div class="point text-black text-sm w-8">VII.</div>
+          <div class="point text-black text-sm w-12 ">{{$this->generateRomanNumberForPoint('INFORMASI LAINNYA')}}.</div>
           <div class="point-content">
             <div class="point-head text-black text-sm mb-1">INFORMASI LAINNYA</div>
             <ul>
@@ -220,7 +268,6 @@
             </ul>
           </div>
         </li>
-
         <li class="flex justify-between mt-24">
           <div class="sign-box  w-full ">
             <p class="text-center text-sm text-black">Mengetahui</p>
@@ -228,7 +275,7 @@
             <div class="mx-10 mt-28 border-b border-dotted border-black"></div>
           </div>
           <div class="sign-box  w-full ">
-            <p class="text-center text-sm text-black">Banjarbaru, 26 Oktober 2023</p>
+            <p class="text-center text-sm text-black">Banjarbaru, {{$this->getTanggalSekarang()}}</p>
             <p class="text-center text-sm text-black">Petugas Penerima</p>
             <div class="mx-10 mt-28 border-b border-dotted border-black"></div>
           </div>
