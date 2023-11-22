1670e269a0efbc14a8cb427fdaba20f827e42f80
Rama Noor Rizki <ramanoorrizki@gmail.com>
2023-11-22 13:56:13 +0800

    fix(fe): add icon log out

--- resources/views/components/cards/card-header-admin-dashboard.blade.php
+++ resources/views/components/cards/card-header-admin-dashboard.blade.php
@@ -2,8 +2,10 @@
   <div class="header-content flex items-center justify-between md:mb-2">
     <h1 class="text-base md:text-3xl  h-full font-bold">HALAMAN UTAMA</h1>
   </div>
-  <a class="border-2  border-dark-grey rounded-md text-xs py-1 px-2 text-dark-grey font-bold flex flex-wrap content-center justify-center"
-    href="/logout">
-    Keluar
+  <a class="border-2  border-dark-grey hover:bg-light-2  rounded-md text-xs py-1 px-2 text-dark-grey font-bold flex flex-wrap content-center justify-center" href="/logout">
+    <span class="text-dark-grey">Keluar</span>
+    <svg class="fill-dark-grey" xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="24">
+      <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+    </svg>
   </a>
 </div>
